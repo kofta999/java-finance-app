75af96289904f2157a87089765d2f7a0c8810ca9
kofta999 <mostafaxxx555@gmail.com>
2026-02-17 17:13:18 +0200

    refactor: convert TransactionParser into an interface with impl

---
+++ src/main/java/com/kofta/app/transaction/CsvTransactionParser.java
@@ -0,0 +1,42 @@
+package com.kofta.app.transaction;
+
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class CsvTransactionParser implements TransactionParser {
+
+    @Override
+    public List<ParsedTransaction> from(InputStream stream) {
+        var mapper = new CsvMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        var schema = CsvSchema.builder()
+            .addColumn("date")
+            .addColumn("description")
+            .addColumn("amount")
+            .addColumn("category")
+            .setUseHeader(true)
+            .build();
+
+        try (
+            MappingIterator<ParsedTransaction> iterator = mapper
+                .readerFor(ParsedTransaction.class)
+                .with(schema)
+                .readValues(stream);
+        ) {
+            return iterator.readAll();
+        } catch (IOException e) {
+            throw new TransactionParsingException(
+                "Failed to parse transactions from file",
+                e
+            );
+        }
+    }
+}
--- src/main/java/com/kofta/app/transaction/TransactionParser.java
+++ src/main/java/com/kofta/app/transaction/TransactionParser.java
@@ -1,42 +1,8 @@
 package com.kofta.app.transaction;
 
-import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 
-public class TransactionParser {
-
-    public static List<Transaction> fromCsvFile(InputStream stream)
-        throws TransactionParsingException {
-        var mapper = new CsvMapper();
-        mapper.registerModule(new JavaTimeModule());
-        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-        var schema = CsvSchema.builder()
-            .addColumn("date")
-            .addColumn("description")
-            .addColumn("amount")
-            .addColumn("category")
-            .setUseHeader(true)
-            .build();
-
-        try (
-            MappingIterator<Transaction> iterator = mapper
-                .readerFor(Transaction.class)
-                .with(schema)
-                .readValues(stream);
-        ) {
-            return iterator.readAll();
-        } catch (IOException e) {
-            throw new TransactionParsingException(
-                "Failed to parse transactions from file",
-                e
-            );
-        }
-    }
+public interface TransactionParser {
+    public List<ParsedTransaction> from(InputStream stream);
 }

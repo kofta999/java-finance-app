d74e2c3784252120d4852fb3f010e51d4e0ad1c8
kofta999 <mostafaxxx555@gmail.com>
2026-02-21 11:41:32 +0200

    chore: remove extra import

--- src/test/java/com/kofta/app/transaction/CsvTransactionParserTest.java
+++ src/test/java/com/kofta/app/transaction/CsvTransactionParserTest.java
@@ -1,7 +1,6 @@
 package com.kofta.app.transaction;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;

1ad10f234cdd369318ad0559e310ab82b282f647
kofta999 <mostafaxxx555@gmail.com>
2026-02-22 16:51:03 +0200

    refactor: wire sqlite to repos

--- src/main/java/com/kofta/app/App.java
+++ src/main/java/com/kofta/app/App.java
@@ -3,12 +3,13 @@ package com.kofta.app;
 import com.kofta.app.account.Account;
 import com.kofta.app.account.AccountRepository;
 import com.kofta.app.account.AccountServiceImpl;
-import com.kofta.app.account.InMemoryAccountRepository;
+import com.kofta.app.account.SqliteAccountRepository;
+import com.kofta.app.database.DatabaseConnectionManager;
 import com.kofta.app.finance.FinanceServiceImpl;
 import com.kofta.app.transaction.CsvTransactionParser;
-import com.kofta.app.transaction.InMemoryTransactionRepository;
+import com.kofta.app.transaction.SqliteTransactionRepository;
 import com.kofta.app.ui.FinanceConsole;
-import com.kofta.app.user.InMemoryUserRepository;
+import com.kofta.app.user.SqliteUserRepository;
 import com.kofta.app.user.User;
 import com.kofta.app.user.UserRepository;
 import com.kofta.app.user.UserServiceImpl;
@@ -71,7 +72,11 @@ public class App {
     }
 
     public static void main(String[] args) {
-        try {
+        try (
+            var dbManager = new DatabaseConnectionManager(
+                "jdbc:sqlite:finance.db"
+            )
+        ) {
             InputStream stream = App.class.getClassLoader().getResourceAsStream(
                 "input.csv"
             );
@@ -80,9 +85,15 @@ public class App {
                 throw new IOException("File not found in classpath");
             }
 
-            var userRepository = new InMemoryUserRepository();
-            var accountRepository = new InMemoryAccountRepository();
-            var transactionRepository = new InMemoryTransactionRepository();
+            // var userRepository = new InMemoryUserRepository();
+            // var accountRepository = new InMemoryAccountRepository();
+            // var transactionRepository = new InMemoryTransactionRepository();
+            var connection = dbManager.getConnection();
+            var userRepository = new SqliteUserRepository(connection);
+            var accountRepository = new SqliteAccountRepository(connection);
+            var transactionRepository = new SqliteTransactionRepository(
+                connection
+            );
 
             var transactionParser = new CsvTransactionParser();
 
